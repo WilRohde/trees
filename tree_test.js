0d956ff1f957d1595f4e854a9df5709f834d8287
WilRohde <warohde@gmail.com>
2022-04-11 02:29:28 -0400

    changes to utilities

---
+++ tree_test.js
@@ -0,0 +1,12 @@
+import {BTNode} from './trees1.js';
+import {BST} from './trees1.js';
+
+var newTree = new BST();//.add(33).add(99).add(144).add(5).add(39).add(78).add(16);
+newTree.Verbose(true);
+newTree.add(24).add(33).add(99).add(144).add(5).add(39).add(78).add(16).add(16).add(122).add(99).add(19).add(41).add(56).add(67).add(65).add(201).add(66).add(501).add(432).add(787);
+console.log(newTree.size());
+console.log(`newTree contains 999: ${newTree.contains(999)}`);
+console.log(`newTree contains 39: ${newTree.contains(39)}`);
+
+console.log(`newTree.min() = ${newTree.min()}`);
+console.log(`newTree.max() = ${newTree.max()}`);
--- trees1.js
+++ trees1.js
@@ -1,25 +1,47 @@
-class BTNode {
+export class BTNode {
 constructor(value) {
-    this.val = value;
+    this.value = value;
     this.left = null;
     this.right = null;
     }
+
+    print() {
+        return `value = ${this.value}, this.left = ${this.left}, this.right = ${this.right}`;
+    }
 }
 
-class BST {
+export class BST {
     constructor() {
         this.root = null;
+        this.verbose = false;
     }
     // add methods here...
 
+    Verbose(value) {
+        if (value != true) {
+            this.verbose = false;
+        }
+        else {
+            this.verbose = true;
+        }
+    }
+
     add(val) {
         var newNode = new BTNode(val);
+        if (this.verbose == true) {
+            console.log(`created new node ${newNode}`);
+        }
         if (this.root == null) {
+            if (this.verbose == true) {
+                console.log(`setting ${val} as root node`);
+            }
             this.root = newNode;
             return this;
         }
 
         // new node isn't the root so we have to figure out where it goes
+        this.compare(this.root, newNode);
+        return this;
     }
 
     compare(prevNode,newNode) {
@@ -66,24 +88,24 @@ class BST {
             return true;
         }
         if (prevNode.value < value) {
-            // going to the left
-            if (prevNode.left == null) {
-                // it's not there
-                return false;
-            }
-            else {
-                // going left
-                return this.search(prevNode.left,value);
-            }
-        }
-        else {
             // going to the right
             if (prevNode.right == null) {
                 // it's not there
                 return false;
             }
             else {
+                // going right
                 return this.search(prevNode.right,value);
+            }
+        }
+        else {
+            // going to the left
+            if (prevNode.left == null) {
+                // it's not there
+                return false;
+            }
+            else {
+                return this.search(prevNode.left,value);
             }
         }
     }
@@ -95,23 +117,79 @@ class BST {
             return this;
         }
         var curNode = this.root;
-        var value = curNode.value;
+        if (this.verbose == true) {
+            console.log(`root value = ${this.root.value}`);
+        }
         // just keep going left
-        return this.searchLeft(curNode,value);
+        return this.searchLeft(curNode);
     }
 
-    searchLeft(curNode,value) {
+    searchLeft(curNode) {
         if (curNode.left == null) {
             // this is the end of the left tree - should be the minimum value as long
             // as the tree is built correctly
+            if (this.verbose == true) {
+                console.log(`searchLeft value = ${curNode.value}`);
+            }
             return curNode.value;
         }
         else {
             // otherwise we keep going left
             var leftNode = curNode.left;
-            var value = curNode.value;
-            return this.searchLeft(leftNode,value);
+            return this.searchLeft(leftNode);
         }
+    }
+
+    max() {
+        // find the maximum amount in the tree. should just be able to follow the right-hand
+        // branch all the way down until it stops
+        if (this.root == null) {
+            return this;
+        }
+        var curNode = this.root;
+        // just keep going left
+        return this.searchRight(curNode);
+    }
+
+    searchRight(curNode) {
+        if (curNode.right == null) {
+            // end of the right branch, should be our max value
+            return curNode.value;
+        }
+        else {
+            // keep going right...
+            var rightNode = curNode.right;
+            return this.searchRight(rightNode);
+        }
+    }
+
+    size() {
+        // figure out how many nodes are in the tree
+        if (this.root == null) {
+            return 0;
+        }
+        // add one for the root node...
+        if (this.verbose == true) {
+            console.log(`in size(), root node = ${this.root}`);
+        }
+        return 1 + this.nodeCount(this.root);
+    }
+
+    nodeCount(curNode) {
+        if (this.verbose == true) {
+            console.log(`entered nodeCount, curNode = ${curNode}`);
+        }
+        var subNodeCount = 0
+        if (curNode == null) {
+            return subNodeCount;
+        }
+        if (curNode.left != null) {
+            subNodeCount++;
+        }
+        if (curNode.right != null) {
+            subNodeCount++
+        }
+        return subNodeCount + this.nodeCount(curNode.right) + this.nodeCount(curNode.left);
     }
 
     isEmpty() {
@@ -124,3 +202,4 @@ class BST {
         }
     }
 }
+
